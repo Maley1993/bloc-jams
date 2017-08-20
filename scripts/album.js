4178cb7369879d7783af0f69d8e2190604171b6e
Maley1993 <maley1993@gmail.com>
2017-08-19 18:40:02 -0700

    fixed album.js and moved album objects to fixutres.js

--- scripts/album.js
+++ scripts/album.js
@@ -1,48 +1,3 @@
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'}
-    ]
-};
-
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
-    songs: [
-        { title: 'Hello, Operator?', duration: '1:01' },
-        { title: 'Ring, ring, ring', duration: '5:01' },
-        { title: 'Fits in your pocket', duration: '3:21'},
-        { title: 'Can you hear me now?', duration: '3:14' },
-        { title: 'Wrong phone number', duration: '2:15'}
-    ]
-};
-
-var albumWorkShop = {
-    title: 'The Workshop',
-    artist: 'Leroy Jenkins',
-    label: 'Workshop',
-    year: '1993',
-    albumArtUrl: 'assets/images/album_covers/07.png',
-    songs: [
-        { title: 'Woodwork', duration: '4:18' },
-        { title: 'Working all day', duration: '5:14' },
-        { title: 'Still Workin', duration: '3:41' },
-        { title: 'Almost Close', duration: '3:28'},
-        { title: 'Finally off', duration: '4:15'}
-    ]
-};
-
 var changeAlbum = function() {
   var albums = [albumPicasso, albumMarconi, albumWorkShop];
   var currentAlbum = document.getElementsByClassName("album-view-title")[0].innerText;
@@ -84,9 +39,8 @@ var createSongRow = function(songNumber, songName, songLength) {
 		// Switch from Pause -> Play button to pause currently playing song.
 		$(this).html(playButtonTemplate);
 		currentlyPlayingSong = null;
-	}
-       };
-
+	  }
+  };
 
   var onHover = function(event) {
     var songNumberCell = $(this).find('.song-item-number');
@@ -97,6 +51,7 @@ var createSongRow = function(songNumber, songName, songLength) {
     }
 
   };
+
   var offHover = function(event) {
     var songNumberCell = $(this).find('.song-item-number');
       var songNumber = songNumberCell.attr('data-song-number');
@@ -113,24 +68,22 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 var setCurrentAlbum = function(album) {
      // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+     var $albumTitle = $('.album-view-title');
+     var $albumArtist = $('.album-view-artist');
+     var $albumReleaseInfo = $('.album-view-release-info');
+     var $albumImage = $('.album-cover-art');
+     var $albumSongList = $('.album-view-song-list');
 
-     // #2
-     albumTitle.firstChild.nodeValue = album.title;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
+    $albumTitle.text(album.title);
+    $albumArtist.text(album.artist);
+    $albumReleaseInfo.text(album.year + ' ' + album.label);
+    $albumImage.attr('src', album.albumArtUrl);
 
-     // #3
-     albumSongList.innerHTML = '';
+     $albumSongList.empty();
 
-     // #4
-     for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+    for (var i = 0; i < album.songs.length; i++) {
+      var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+      $albumSongList.append($newRow);
      }
  };
 
---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var albumWorkShop = {
+    title: 'The Workshop',
+    artist: 'Leroy Jenkins',
+    label: 'Workshop',
+    year: '1993',
+    albumArtUrl: 'assets/images/album_covers/07.png',
+    songs: [
+        { title: 'Woodwork', duration: '4:18' },
+        { title: 'Working all day', duration: '5:14' },
+        { title: 'Still Workin', duration: '3:41' },
+        { title: 'Almost Close', duration: '3:28'},
+        { title: 'Finally off', duration: '4:15'}
+    ]
+};
